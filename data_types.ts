74bce74300acdb0a95b59666251103269cc63bd0
Nilesh Wani <nileshwaani@gmail.com>
2025-10-01 11:23:42 +0530

    Added TypeScript file showing basic data types

---
+++ data_types.ts
@@ -0,0 +1,20 @@
+// Primitive types: number, string, boolean
+let age: number = 30;
+age = 31;
+
+let userName: string = 'Max';
+userName = 'Manu';
+
+let isInstructor: boolean;
+isInstructor = true;
+
+// More complex types: arrays, objects
+let hobbies: string[] = [ 'Sports', 'Cooking' ];
+hobbies = [ 'Reading', 'Traveling' ];
+
+let person: { name: string; age: number };
+person = { name: 'Max', age: 32 };
+
+let people: { name: string; age: number }[];
+people = [ { name: 'Max', age: 32 }, { name: 'Manu', age: 29 } ];
+
